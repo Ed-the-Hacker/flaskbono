cbf4181f9dc0c4b12128d6c4d49288510cba5dc8
Edwin Ariel Sanabria <edwin.sanabria.mosquera@udla.edu.ec>
2020-01-17 18:19:12 -0500

    Holad3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
 app.config['SECRET_KEY']='Esto es un secreto'
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database/video.db'
+app.config['DATABASE_URL']
 
 db=SQLAlchemy(app)
 
