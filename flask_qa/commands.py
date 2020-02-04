4ba1eb84c768ab1f2e23b3abba31d30ba87bb212
Edwin Ariel Sanabria <edwin.sanabria.mosquera@udla.edu.ec>
2020-02-04 17:41:57 -0500

    comando

--- flask_qa/commands.py
+++ flask_qa/commands.py
@@ -8,3 +8,14 @@ from .models import Usuario, Bono
 @with_appcontext
 def create_tables():
     db.create_all()
+
+def create_data():
+    usuario1 = Usuario(1,"daniel", "gerencia")
+    bono1 = Bono(1, 20.5, "gerencia")
+    usuario2 = Usuario(2,"pedro", "sistemas")
+    bono2 = Bono(1, 50, "sistemas")
+    db.session.add(usuario1)
+    db.session.add(bono1)
+    db.session.add(usuario2)
+    db.session.add(bono2)
+    db.session.commit()
